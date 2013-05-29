566519de9caa750c528e2f34daa3d7618e876960
John Lees-Miller <jdleesmiller@gmail.com>
2013-05-29 20:20:15 +0100

    version bump

--- lib/omniauth-figshare/version.rb
+++ lib/omniauth-figshare/version.rb
@@ -2,7 +2,7 @@ module OmniAuth
   module Figshare
     VERSION_MAJOR = 0
     VERSION_MINOR = 0
-    VERSION_PATCH = 1
+    VERSION_PATCH = 2
     VERSION = [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH].join('.')
   end
 end
