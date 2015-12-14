e2641a0ccf8d9bc9e0191bdfedeb8397ccb477ab
Tim Alby <timothee.alby@gmail.com>
2015-12-14 15:07:49 +0000

    bump version to 0.1.1

--- lib/omniauth-figshare/version.rb
+++ lib/omniauth-figshare/version.rb
@@ -2,7 +2,7 @@ module OmniAuth
   module Figshare
     VERSION_MAJOR = 0
     VERSION_MINOR = 1
-    VERSION_PATCH = 0
+    VERSION_PATCH = 1
     VERSION = [VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH].join('.')
   end
 end
